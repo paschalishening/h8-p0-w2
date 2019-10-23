8495af7e4d6ccb3d015eb8e750dfbba97f588290
Paschalis Hening <paschalpraset@gmail.com>
2019-10-23 15:45:29 +0700

    tugas swtich case

---
+++ exercise4.js
@@ -0,0 +1,22 @@
+var tanggal = 2; // masukan angka 1-31
+var bulan = 13; // masukan angka 1-12
+var tahun = 1996; //masukan variabel tahun dari 1900 -2000
+
+switch(bulan){
+case 1: {console.log('tanggal hari ini adalah' + '' + 'tanggal' + '' +'Jan' + '' +tahun); break;}
+    case 2: { console.log('tanggal hari ini adalah' + '' + 'tanggal' + '' + 'Feb' + '' + tahun); break; }
+    case 3: { console.log('tanggal hari ini adalah' + '' + 'tanggal' + '' + 'March' + '' + tahun); break; }
+    case 4:  { console.log('tanggal hari ini adalah' + '' + 'tanggal' + '' + 'Apr' + '' + tahun); break; }
+    case 5: { console.log('tanggal hari ini adalah' + '' + 'tanggal' + '' + 'Mei' + '' + tahun); break; }
+    case 6: {console.log('tanggal hari ini adalah' + '' + 'tanggal' + '' +'June' + '' +tahun); break;}
+    case 7: {console.log('tanggal hari ini adalah' + '' + 'tanggal' + '' +'Jul' + '' +tahun); break;}
+    case 8: {console.log('tanggal hari ini adalah' + '' + 'tanggal' + '' +'Aug' + '' +tahun); break;}
+    case 9: {console.log('tanggal hari ini adalah' + '' + 'tanggal' + '' +'Sept' + '' +tahun); break;}
+    case 10: {console.log('tanggal hari ini adalah' + '' + 'tanggal' + '' +'Oct' + '' +tahun); break;}
+    case 11: {console.log('tanggal hari ini adalah' + '' + 'tanggal' + '' +'Nov' + '' +tahun); break;}
+    case 12: {console.log('tanggal hari ini adalah' + '' + 'tanggal' + '' +'Des' + '' +tahun); break;}
+    default: {
+        console.log("masukan tanggal, bulan dan tahun!");
+    }
+}
+
