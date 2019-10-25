cfd26ec1c5eca15c07a276d0b46e1983379a2ac5
Paschalis Hening <paschalpraset@gmail.com>
2019-10-25 17:37:43 +0700

    Tugas Exercise 11-13

---
+++ exercise13.js
@@ -0,0 +1,27 @@
+
+function xo(str) {
+    var x = 0;
+    var o = 0; 
+for(var i=0;i<str.length; i++)
+    if(str[i] === 'x'){
+        x+=1;
+    }else if(str[i] === 'o'){
+        o+=1;
+    }else{
+        return 'masukan x dan 0'
+    }
+    if (x === o) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // 
+
+
